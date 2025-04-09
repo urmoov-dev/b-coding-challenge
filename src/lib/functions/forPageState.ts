7820f5beb3c6104178a2bdf8629623b770f30b8e
Jorge Linares <urmoovdev@gmail.com>
2025-04-09 10:21:56 -0400

    Fix: removed console.log

--- src/lib/functions/forPageState.ts
+++ src/lib/functions/forPageState.ts
@@ -2,15 +2,13 @@ import { pushState, replaceState } from "$app/navigation";
 import { page } from "$app/state";
 import { setToValue } from "./forObjects";
 
-export function setPageState(value: any, path:string, method = "push") {
+export async function setPageState(value: any, path:string, method = "push") {
     
     const newState = {...page.state}
     setToValue(newState, value, path)
 
     if (method === "push") pushState('', newState)
     if (method === "replace") replaceState('', newState)
-
-    console.log(page.state)
 }
 
 export type PageStateKey = keyof typeof page.state 
