91dfeff25da9a9b01e4bb24c48bb2b62f2433fdb
Jorge Linares <urmoovdev@gmail.com>
2025-04-09 23:50:08 -0400

    Feat: Navigation bar at the bottom for mobile ergonomics

---
+++ src/routes/components/ThumbNav.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { page } from "$app/state";
+	import { getLayoutState } from "$lib/components/layout-state.svelte";
+	import { setPageState, type PageStateKey } from "$lib/functions/forPageState";
+	import { faList, faTable } from "@fortawesome/free-solid-svg-icons";
+	import Fa from "svelte-fa";
+
+    const layout = getLayoutState()
+</script>
+
+{#if layout.portrait || layout.width <= 1000}
+    <div class="thumb-navigation flex justify-evenly w-full">
+        <button class="flex items-center gap-3"
+            onclick={() => {setPageState("table", "view", "push")}}
+            class:selected={page.state["view" as PageStateKey] === "table"}
+        >
+            <Fa icon={faTable}/>
+        </button>
+        <button class="flex items-center gap-3"
+            onclick={() => {setPageState("list", "view", "push")}}
+            class:selected={page.state["view" as PageStateKey] === "list"}
+        >
+            <Fa icon={faList}/>
+        </button>
+    </div>
+{/if}
+
+<style>
+    .thumb-navigation {
+        height: 8dvh;
+    }
+    
+    button {
+        font-size: 2rem;
+        padding: 0.2rem 0.9rem;
+        border-radius: 1rem;
+    }
+    .selected {
+        background-color: var(--color-theme-1);
+    }
+
+</style>
